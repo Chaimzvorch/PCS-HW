684f1d12b6139930e3c4eb947d0a9e2e8b6aa661
Chaim Vorchheimer <chaimzvorch@gmail.com>
2025-06-18 19:57:43 -0400

    Add files via upload

---
+++ Shirt.cs
@@ -0,0 +1,38 @@
+﻿using System.Drawing;
+
+namespace shirt49 {
+    public class Shirt {
+
+    }
+    public class CreateShirt {
+        public string Color { get; set; }
+        public string Pattern { get; set; }
+        public CreateShirt() {
+
+        }
+        public CreateShirt(string color, string pattern) {
+            Pattern = pattern;
+            Color = color;
+
+        }
+       public static void Print(string col, string pat) {
+            Console.WriteLine($"The color of the shirt is {col} and the pattern is {pat}");
+        }
+
+        static void Main(string[] args) {
+
+            string[] color = { "red", "blue", "green" };
+            string[] pattern = { "striped", "checked", "plain" };
+            Shirt shirt = new Shirt();
+            for (int i = 0; i < color.Length; i++)
+            {
+                for(int j = 0; i < pattern.Length; i++) {
+                    Print(color[i], pattern[i]);
+                };
+            }
+
+        }
+           
+        
+    }
+}
