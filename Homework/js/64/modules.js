84d4cca42b4a0a5a46f7c7cf99793bea39c58d4e
Chaimzvorch <chaimzvorch@gmail.com>
2025-08-31 19:49:48 -0400

    HW64

---
+++ Homework/js/64/modules.js
@@ -0,0 +1,37 @@
+const dayofWeek = (function () {
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Shabbos'];
+
+    return {
+        getDayName(index) {
+            return days[index - 1];
+        },
+        getDayNumber(day) {
+            return days.findIndex(d => d === day) + 1;
+        },
+    };
+}());
+
+console.log(dayofWeek.getDayName(5))
+console.log(dayofWeek.getDayNumber("Shabbos"))
+
+const interestCalculator = (function () {
+    let rate = 0;
+    let year = 0;
+
+    return {
+        setRate(x) {
+            rate = x;
+        },
+        setYears(y) {
+            year = y;
+        },
+        calculateInterest(principal) {
+            return (principal * rate * year) / 100;
+
+        }
+    };
+}());
+interestCalculator.setRate(2.5);
+interestCalculator.setYears(3.5);
+const interest = interestCalculator.calculateInterest(200);
+console.log(interest)
